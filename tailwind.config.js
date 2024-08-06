54741cb5dbfea8cbdbcb4a2a5e7f62885de8daef
Viviane Alves <125809555+viviianealvesc@users.noreply.github.com>
2024-08-06 10:05:04 -0300

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        'none': '0',  
+      },
+    },
+  },
+  plugins: [],
+}
+
